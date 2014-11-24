1795ebecc6a008e58cc837ec0825f47e21ad3730
FingerLiu <solo_o@foxmail.com>
2014-11-24 14:49:37 +0800

    add real renren.js

--- xpi/handlers/renren.js
+++ xpi/handlers/renren.js
@@ -1,11 +1,14 @@
 // Authors:
-//   Amr N. Tamimi <amr@rushthinking.com>
-register({
-  name: "reddit",
-  domains: [ 'www.reddit.com' ],
-  sessionCookieNames: [ 'reddit_session' ],
-  identifyUser: function () {
-    var resp = this.httpGet(this.siteUrl);
-    this.userName = resp.body.querySelector('.user a').textContent;
-  }
-});
+//   Eric Butler <eric@codebutler.com>
+//   Ian Gallagher <crash@neg9.org>
+    register({  
+      name: 'renren.com', 
+      url: 'http://www.renren.com/', 
+      domains: [ 'renren.com' ], 
+      sessionCookieNames: [ 'p', 't' ], 
+      identifyUser: function() { 
+        var site = this.httpGet(this.siteUrl); 
+       this.userName = site.body.querySelector('a.name').innerHTML; 
+       // this.userAvatar = resp.body.querySelector('div.figure').src; //another query selector to grab the image (optional)   
+      }  
+    });  
